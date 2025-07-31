3978ef4dad146b695924497fdb08934d4d889498
Lucas Fernandes Souza <ludosouzabr@gmail.com>
2025-07-31 12:54:37 -0300

    feat: add github pages library and deploy configuration

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import Main from './pages/main'
 
-const router = createBrowserRouter([{ path: '/', element: <Main /> }])
+const router = createBrowserRouter([{ path: '/', element: <Main /> }], {
+  basename: '/my-portifolio',
+})
 
 export default router
