751fb39e81f5ad1f0bf1235d6db047c5c9de914f
Lucas Fernandes Souza <ludosouzabr@gmail.com>
2025-09-05 03:50:17 -0300

    chore: remove unused component - Accordion

--- src/components/Accordion/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as AccordionPrimitive from '@radix-ui/react-accordion'
-
-import AccordionContent from './AccordionContent'
-import AccordionItem from './AccordionItem'
-import AccordionTrigger from './AccordionTrigger'
-
-const Accordion = AccordionPrimitive.Root
-
-export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
